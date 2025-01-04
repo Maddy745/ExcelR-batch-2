f5e64b8e5d2180f02b51aff2791c9ddeb9e41618
Maddy745 <madhavrega@gmail.com>
2025-01-04 10:32:24 +0530

    Add files via upload

---
+++ demo006.java
@@ -0,0 +1,11 @@
+public class demo006 {
+    public static void main(String[] args) {
+        String name = "Madhav";
+        String pwd = "Madhav@123";
+        while ((name == "Madhav")&&(pwd == "Madhav@123")){
+            System.out.println("Hello Madhav welcome to malla reddy university");
+            break;
+        }
+    }
+    
+}
